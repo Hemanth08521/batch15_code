3ff0e3a9af1d5f728b2ca3edb0cbd0207a14e050
Hemanth Vonteru <vonteruhemanthkumar@gmail.com>
2026-03-30 14:09:25 +0000

    commit

--- models/Staging/leftouterjoin.sql
+++ models/Staging/leftouterjoin.sql
@@ -1,5 +1,6 @@
 {{config(materialized='table')}}
 
-select a.*,b.* from {{source('datafeed_shared_schema','samp1')}} a 
-left join 
-{{source('datafeed_shared_schema','samp2')}} b on a.id=b.id
+select a.id as pr_id,
+a.product,b.* 
+from {{source('datafeed_shared_schema','samp1')}} a 
+left join {{source('datafeed_shared_schema','samp2')}} b on a.id=b.id
